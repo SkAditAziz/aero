94535158853973088defbab91683851ca947cbeb
SkAditAziz <skaditaziz@gmail.com>
2023-11-09 13:21:54 +0600

    Implemented required methods in AirportDAO

--- src/main/java/dev/example/aero/dao/AirportDao.java
+++ src/main/java/dev/example/aero/dao/AirportDao.java
@@ -2,6 +2,10 @@ package dev.example.aero.dao;
 
 import dev.example.aero.model.Airport;
 
+import java.util.List;
+
 public interface AirportDao {
     void save(Airport airport);
+    List<Airport> findAll();
+    Airport findByID(String code);
 }
--- src/main/java/dev/example/aero/dao/daoImpl/AirportDAOImpl.java
+++ src/main/java/dev/example/aero/dao/daoImpl/AirportDAOImpl.java
@@ -3,21 +3,33 @@ package dev.example.aero.dao.daoImpl;
 import dev.example.aero.dao.AirportDao;
 import dev.example.aero.model.Airport;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class AirportDAOImpl implements AirportDao {
 
+    @Autowired
     private EntityManager entityManager;
-
-    public AirportDAOImpl(EntityManager entityManager){
-        this.entityManager = entityManager;
-    }
 
     @Override
     @Transactional
     public void save(Airport airport) {
         entityManager.persist(airport);
+    }
+
+    @Override
+    public List<Airport> findAll() {
+        TypedQuery<Airport> sql = entityManager.createQuery("select a from Airport a", Airport.class);
+        return sql.getResultList();
+    }
+
+    @Override
+    public Airport findByID(String code) {
+        return entityManager.find(Airport.class, code);
     }
 }
