e96b9efe0fb47ded2ef5760a40d208ab8bd7a3d5
SkAditAziz <skaditaziz@gmail.com>
2023-11-10 15:41:20 +0600

    Added CustomFlightIDGenerator

---
+++ src/main/java/dev/example/aero/custom/CustomFlightIDGenerator.java
@@ -0,0 +1,30 @@
+package dev.example.aero.custom;
+
+import dev.example.aero.model.Flight;
+import jakarta.persistence.NoResultException;
+import jakarta.persistence.TypedQuery;
+import org.hibernate.engine.spi.SharedSessionContractImplementor;
+import org.hibernate.id.IdentifierGenerator;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+public class CustomFlightIDGenerator implements IdentifierGenerator {
+    @Override
+    public Object generate(SharedSessionContractImplementor sharedSessionContractImplementor, Object o) {
+        Flight currentFlight = (Flight) o;
+        String fromAirportCode = currentFlight.getFromAirport().getCode();
+        String toAirportCode = currentFlight.getToAirport().getCode();
+        String customIDPrefix = fromAirportCode + "-" + toAirportCode + "-";
+
+        TypedQuery<Integer> getMaxIDQuery = sharedSessionContractImplementor.createQuery(
+                "SELECT MAX(SUBSTRING(f.id, :startIdx)) FROM Flight f WHERE f.id LIKE :customIDPrefix", Integer.class);
+        getMaxIDQuery.setParameter("startIdx",customIDPrefix.length()+1);
+        getMaxIDQuery.setParameter("customIDPrefix", customIDPrefix + "%");
+
+        String maxIDString = (String) getMaxIDQuery.getResultList().toArray()[0];
+        int sequence = (maxIDString != null)? (Integer.parseInt(maxIDString) + 1): 1;
+
+        return customIDPrefix + String.format("%03d", sequence);
+    }
+}
