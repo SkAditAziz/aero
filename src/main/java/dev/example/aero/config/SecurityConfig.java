7c8c0e7183f3bff5f37f5c7dc9f7064d3f79fece
SkAditAziz <skaditaziz@gmail.com>
2023-11-16 02:17:09 +0600

    Added Password encoder using BCrypt disabled default authentication, csrf after using starter-security

---
+++ src/main/java/dev/example/aero/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package dev.example.aero.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((authorizeHttpRequests) ->
+                authorizeHttpRequests.anyRequest()
+                .permitAll())
+                .csrf(AbstractHttpConfigurer::disable);
+        return http.build();
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
