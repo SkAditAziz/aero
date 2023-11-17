9b91e7c3f93670b51c48e5e0fb65c6b80ff6a5da
SkAditAziz <skaditaziz@gmail.com>
2023-11-18 01:02:09 +0600

    Added method to update seat allocation after confirming ticket

--- src/main/java/dev/example/aero/service/TicketService.java
+++ src/main/java/dev/example/aero/service/TicketService.java
@@ -1,6 +1,10 @@
 package dev.example.aero.service;
 
+import dev.example.aero.model.Flight;
+import dev.example.aero.model.FlightSchedule;
+import dev.example.aero.model.SeatInfo;
 import dev.example.aero.model.Ticket;
+import dev.example.aero.repository.FlightScheduleRepository;
 import dev.example.aero.repository.TicketRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,7 +12,30 @@ import org.springframework.stereotype.Service;
 public class TicketService {
     @Autowired
     private TicketRepository ticketRepository;
+    @Autowired
+    private FlightScheduleRepository flightScheduleRepository;
     public void issueTicket(Ticket t) {
         ticketRepository.save(t);
+        updateSeatAllocation(t);
+    }
+
+    private void updateSeatAllocation(Ticket ticket) {
+        FlightSchedule desiredSchedule = ticket.getFlightSchedule();
+
+        Flight flight = ticket.getFlight();
+        Flight desiredFlight = (Flight) desiredSchedule.getFlights().stream()
+                .filter(f -> f.getId().equals(flight.getId()))
+                .findFirst()
+                .orElse(null);
+
+        assert desiredFlight != null;
+        SeatInfo seatInfo = desiredFlight.getSeatInfoList().stream()
+                .filter(s -> s.getSeatClassType() == ticket.getSeatClassType())
+                .findFirst()
+                .orElse(null);
+
+        assert seatInfo != null;
+        seatInfo.setAvailableSeats(seatInfo.getAvailableSeats() - ticket.getTotalSeats());
+        flightScheduleRepository.save(desiredSchedule);
     }
 }
