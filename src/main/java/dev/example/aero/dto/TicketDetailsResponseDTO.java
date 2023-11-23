327bde030e0cdc9ac560bf4a398c041c1d6d544a
SkAditAziz <skaditaziz@gmail.com>
2023-11-23 19:13:15 +0600

    Added TicketDetailsResponseDTO and mapper classes

---
+++ src/main/java/dev/example/aero/dto/TicketDetailsResponseDTO.java
@@ -0,0 +1,25 @@
+package dev.example.aero.dto;
+
+import dev.example.aero.model.Enumaration.SeatClassType;
+import dev.example.aero.model.Enumaration.TicketStatus;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class TicketDetailsResponseDTO {
+    private String id;
+    private LocalDate date;
+    private String from;
+    private String to;
+    private SeatClassType seatClassType;
+    private int seats;
+    private double totalFare;
+    private TicketStatus ticketStatus;
+}
---
+++ src/main/java/dev/example/aero/dto/mapper/TicketDetailsResponseDTOMapper.java
@@ -0,0 +1,22 @@
+package dev.example.aero.dto.mapper;
+
+import dev.example.aero.dto.TicketDetailsResponseDTO;
+import dev.example.aero.model.Ticket;
+
+import java.util.function.Function;
+
+public class TicketDetailsResponseDTOMapper implements Function<Ticket,TicketDetailsResponseDTO> {
+    @Override
+    public TicketDetailsResponseDTO apply(Ticket ticket) {
+        return new TicketDetailsResponseDTO(
+                ticket.getId(),
+                ticket.getFlightSchedule().getFlightDate(),
+                ticket.getFlight().getFromAirport().getCity(),
+                ticket.getFlight().getToAirport().getCity(),
+                ticket.getSeatClassType(),
+                ticket.getTotalSeats(),
+                ticket.getTotalFare(),
+                ticket.getTicketStatus()
+        );
+    }
+}
