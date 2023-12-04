8a729afe51c4b77cdcbf9b4c42477039a69a50de
SkAditAziz <skaditaziz@gmail.com>
2023-12-04 15:47:45 +0600

    Added function to add distance flied after a completing a flight

--- src/main/java/dev/example/aero/repository/PassengerRepository.java
+++ src/main/java/dev/example/aero/repository/PassengerRepository.java
@@ -1,7 +1,9 @@
 package dev.example.aero.repository;
 
+import dev.example.aero.model.Flight;
 import dev.example.aero.model.Passenger;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 public interface PassengerRepository extends JpaRepository<Passenger,Long> {
@@ -11,4 +13,7 @@ public interface PassengerRepository extends JpaRepository<Passenger,Long> {
     String findPasswordByEmail(String email);
     Passenger findByContactNo(String contactNo);
     Passenger findByEmail(String email);
+    @Modifying
+    @Query("UPDATE Passenger p SET p.distanceFlied = p.distanceFlied + (SELECT f.distance FROM Flight f WHERE f=:flight) WHERE p=:passenger")
+    void addDistanceFlied(Passenger passenger, Flight flight);
 }
