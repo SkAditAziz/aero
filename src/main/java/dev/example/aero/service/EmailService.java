9b9d6ebf1508fbef4a93869a998252b4ef74f6f7
SkAditAziz <skaditaziz@gmail.com>
2023-12-07 00:51:00 +0600

    Converted simple email sender to email sender with attachment

--- src/main/java/dev/example/aero/service/EmailService.java
+++ src/main/java/dev/example/aero/service/EmailService.java
@@ -1,26 +1,38 @@
 package dev.example.aero.service;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.MailException;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import java.io.File;
 
 @Service
 public class EmailService {
     @Autowired
     private JavaMailSender javaMailSender;
 
-    public void sendEmail(String to, String sub, String body) {
-        SimpleMailMessage message = new SimpleMailMessage();
-
-        message.setTo(to);
-        message.setSubject(sub);
-        message.setText(body);
-
+    public void sendEmailWithAttachment(String to, String sub, String body, String filePath) {
         try {
+            MimeMessage message = javaMailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+
+            helper.setTo(to);
+            helper.setSubject(sub);
+            helper.setText(body);
+
+            FileSystemResource file = new FileSystemResource(new File(filePath));
+
+            String[] fileNames = filePath.split("/");
+            helper.addAttachment(fileNames[fileNames.length - 1], file);
+
             javaMailSender.send(message);
-        } catch (MailException me) {
+
+        } catch (MailException | MessagingException me) {
             me.printStackTrace();
         }
     }
