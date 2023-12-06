2867029d5c90c322f08e6114092f82539d15a05d
SkAditAziz <skaditaziz@gmail.com>
2023-12-06 23:37:10 +0600

    Implemented functionality for sending an email with necessary info to the passenger

--- src/main/java/dev/example/aero/util/Receiver.java
+++ src/main/java/dev/example/aero/util/Receiver.java
@@ -1,11 +1,23 @@
 package dev.example.aero.util;
 
 import dev.example.aero.model.Ticket;
+import dev.example.aero.service.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.annotation.JmsListener;
 
 public class Receiver {
+    @Autowired
+    private EmailService emailService;
+
     @JmsListener(destination = "messagequeue.q")
     public void sendMail(Ticket ticket) {
-        System.out.println("In receiver\n" + ticket.getId());
+        String to = ticket.getPassenger().getEmail();
+        String sub = "Your Aero Ticket";
+        String body = "Dear " + ticket.getPassenger().getFirstName() + " " +
+                ticket.getPassenger().getLastName() + ",\n\n" +
+                "Your ticket has been confirmed\n\n" +
+                "Thanks,\n" +
+                "Team Aero";
+        emailService.sendEmail(to,sub,body);
     }
 }
