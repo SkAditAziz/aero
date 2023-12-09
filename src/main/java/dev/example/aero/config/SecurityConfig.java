1fa74791199421b19f5b847c8318eb93d9261437
SkAditAziz <skaditaziz@gmail.com>
2023-12-09 16:35:27 +0600

    Added functionality to fix CORs issue

--- src/main/java/dev/example/aero/config/SecurityConfig.java
+++ src/main/java/dev/example/aero/config/SecurityConfig.java
@@ -9,6 +9,10 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -16,6 +20,14 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain filterChain(@NotNull HttpSecurity http) throws Exception {
         http
+                .cors(cors -> cors.configurationSource(request -> {
+                    CorsConfiguration config = new CorsConfiguration();
+                    config.setAllowedOrigins(List.of("*"));
+                    config.setAllowedMethods(List.of("*"));
+                    config.setAllowedHeaders(List.of("*"));
+                    config.setMaxAge(3600L);
+                    return config;
+                }))
                 .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests.anyRequest().permitAll())
                 .csrf(AbstractHttpConfigurer::disable);
         return http.build();
