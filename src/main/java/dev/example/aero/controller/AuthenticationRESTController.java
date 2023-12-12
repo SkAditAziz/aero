0a63a14e15595bf6a244f35d1dea200c9fdadf0b
SkAditAziz <skaditaziz@gmail.com>
2023-12-12 19:52:22 +0600

    Added register and login API that response jwt token

---
+++ src/main/java/dev/example/aero/controller/AuthenticationRESTController.java
@@ -0,0 +1,46 @@
+package dev.example.aero.controller;
+
+import dev.example.aero.model.Passenger;
+import dev.example.aero.security.dto.AuthenticationResponse;
+import dev.example.aero.service.PassengerService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthenticationRESTController {
+    @Autowired
+    private PassengerService passengerService;
+    @PostMapping("/register")
+    public AuthenticationResponse register(@Valid @RequestBody Passenger passenger, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            String errorMessage = bindingResult.getAllErrors().get(0).getDefaultMessage();
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, errorMessage);
+        }
+        return passengerService.register(passenger);
+    }
+
+    @PostMapping("/login")
+    public AuthenticationResponse login(@RequestBody Map<String, Object> loginRequest) {
+        try {
+            return passengerService.authenticate(loginRequest);
+        } catch (Exception e) {
+            // TODO why these message is not being sent as response?
+            if (e instanceof UsernameNotFoundException) {
+                throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Provide correct email or contact number!!!");
+            } else if (e instanceof BadCredentialsException) {
+                throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Incorrect password! Login failed!!!");
+            } else {
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+            }
+        }
+    }
+}
