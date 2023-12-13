3e8d43f9e8f7e9372184225c70a1b5d70b742feb
SkAditAziz <skaditaziz@gmail.com>
2023-12-14 01:43:55 +0600

    Added Role to Passenger Authority

--- src/main/java/dev/example/aero/security/userdetails/PassengerDetails.java
+++ src/main/java/dev/example/aero/security/userdetails/PassengerDetails.java
@@ -17,7 +17,7 @@ public class PassengerDetails implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return Collections.singleton(new SimpleGrantedAuthority("PASSENGER"));
+        return Collections.singleton(new SimpleGrantedAuthority("ROLE_" + passenger.getRole().name()));
     }
 
     @Override
