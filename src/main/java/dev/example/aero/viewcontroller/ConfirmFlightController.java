e883b5644f0c78181a4aaf3ccd62ee1f89bd44c9
SkAditAziz <skaditaziz@gmail.com>
2024-01-02 00:14:30 +0600

    Updated path

--- src/main/java/dev/example/aero/viewcontroller/ConfirmFlightController.java
+++ src/main/java/dev/example/aero/viewcontroller/ConfirmFlightController.java
@@ -1,14 +1,15 @@
 package dev.example.aero.viewcontroller;
 
+import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ConfirmFlightController {
-    @PostMapping("/selectFlight")
-    public String confirmFlight(@RequestParam(name = "scheduleId") Long scheduleId) {
-        System.out.println("In confirm Flight");
+    @PostMapping("/confirmFlight")
+    public String confirmFlight(@RequestParam(name = "scheduleId") Long scheduleId, Model model) {
+        System.out.println("In confirm Flight" + scheduleId);
         return "confirm_flight";
     }
 }
