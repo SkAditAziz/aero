1ef0bc462595fbcf4fa28f13ab75eaa79ef54c9a
SkAditAziz <skaditaziz@gmail.com>
2024-01-03 01:05:02 +0600

    Removed unnecessary ModelAttribute argument for getting view

--- src/main/java/dev/example/aero/viewcontroller/AuthController.java
+++ src/main/java/dev/example/aero/viewcontroller/AuthController.java
@@ -4,12 +4,11 @@ import dev.example.aero.dto.LoginReqDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
 public class AuthController {
     @GetMapping("/login")
-    public String showLoginForm (@ModelAttribute("LoginReqDTO") LoginReqDTO loginReqDTO, Model model) {
+    public String showLoginForm (Model model) {
         model.addAttribute("loginReqDTO", new LoginReqDTO());
         return "login";
     }
