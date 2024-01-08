32b22d584fef73005b35cb6aba270c960e27926c
SkAditAziz <skaditaziz@gmail.com>
2024-01-08 17:26:50 +0600

    Added extraction of passengerId in controller

--- src/main/java/dev/example/aero/viewcontroller/ConfirmFlightController.java
+++ src/main/java/dev/example/aero/viewcontroller/ConfirmFlightController.java
@@ -1,5 +1,8 @@
 package dev.example.aero.viewcontroller;
 
+import dev.example.aero.security.service.UserProvider;
+import dev.example.aero.service.PassengerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,9 +10,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ConfirmFlightController {
+    @Autowired
+    private PassengerService passengerService;
     @PostMapping("/confirmFlight")
     public String confirmFlight(@RequestParam(name = "scheduleId") Long scheduleId, Model model) {
-        System.out.println("In confirm Flight" + scheduleId);
+        Long currentUserId = null;
+        String currentUsername = UserProvider.getCurrentUsername();
+        if (currentUsername != null) {
+            currentUserId = passengerService.getIdByUsername(currentUsername);
+        }
         return "confirm_flight";
     }
 }
