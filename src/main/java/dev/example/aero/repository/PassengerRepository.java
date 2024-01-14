71ac67c08c870121307d0f2d733f8b570715b0a2
SkAditAziz <skaditaziz@gmail.com>
2024-01-14 15:52:06 +0600

    Removed unnecessary Query

--- src/main/java/dev/example/aero/repository/PassengerRepository.java
+++ src/main/java/dev/example/aero/repository/PassengerRepository.java
@@ -7,9 +7,7 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 public interface PassengerRepository extends JpaRepository<Passenger,Long> {
-    @Query("select p.password from Passenger p where p.contactNo =:contactNo")
     String findPasswordByContactNo(String contactNo);
-    @Query("select p.password from Passenger p where p.email =:email")
     String findPasswordByEmail(String email);
     Passenger findByContactNo(String contactNo);
     Passenger findByEmail(String email);
