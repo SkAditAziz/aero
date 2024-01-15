b169324a8ec2915f54d1667f8b0e1bb979015d1e
SkAditAziz <skaditaziz@gmail.com>
2024-01-15 23:22:34 +0600

    Added method to retrieve current logged in passenger

--- src/main/java/dev/example/aero/security/service/UserProvider.java
+++ src/main/java/dev/example/aero/security/service/UserProvider.java
@@ -1,12 +1,17 @@
 package dev.example.aero.security.service;
 
+import dev.example.aero.model.Passenger;
+import dev.example.aero.service.PassengerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 
 public class UserProvider {
+    @Autowired
+    private static PassengerService passengerService;
 
-    public static String getCurrentUsername () {
+    public static String getCurrentUsername() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         String username = null;
         if (authentication != null && authentication.isAuthenticated()) {
@@ -16,5 +21,10 @@ public class UserProvider {
             }
         }
         return username;
+    }
+
+    public static Passenger getCurrentPassenger() {
+        String username = getCurrentUsername();
+        return passengerService.getPassengerByUsername(username);
     }
 }
