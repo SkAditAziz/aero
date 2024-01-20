540f3eb542508f1c9624a7c2cb133e357db5f3a2
SkAditAziz <skaditaziz@gmail.com>
2024-01-21 01:04:45 +0600

    Added validation to avoid for same airport

--- src/main/java/dev/example/aero/dto/FlightSearchReqDTO.java
+++ src/main/java/dev/example/aero/dto/FlightSearchReqDTO.java
@@ -1,6 +1,7 @@
 package dev.example.aero.dto;
 
 import dev.example.aero.model.Enumaration.SeatClassType;
+import jakarta.validation.constraints.AssertTrue;
 import lombok.*;
 import org.jetbrains.annotations.NotNull;
 
@@ -18,6 +19,11 @@ public class FlightSearchReqDTO {
     private LocalDate journeyDate;
     private SeatClassType seatClass;
     private int noPassengers;
+
+    @AssertTrue(message = "Cannot fly to the same place!")
+    public boolean isFormToCodeDifferent() {
+        return !fromCode.equals(toCode);
+    }
 
     public FlightSearchReqDTO(@NotNull LocalDate date) {
         journeyDate = date;
