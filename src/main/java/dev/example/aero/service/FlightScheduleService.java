5b17027986f7722c9329fca22179f36c96d90006
SkAditAziz <skaditaziz@gmail.com>
2024-01-25 21:32:03 +0600

    Restructured function arguments

--- src/main/java/dev/example/aero/service/FlightScheduleService.java
+++ src/main/java/dev/example/aero/service/FlightScheduleService.java
@@ -3,18 +3,26 @@ package dev.example.aero.service;
 import dev.example.aero.dto.FlightDetailsResponseDTO;
 import dev.example.aero.dto.FlightSearchReqDTO;
 import dev.example.aero.dto.mapper.FlightDetailsResponseDTOMapper;
+import dev.example.aero.model.Enumaration.TicketStatus;
 import dev.example.aero.model.Flight;
 import dev.example.aero.model.FlightSchedule;
 import dev.example.aero.repository.FlightRepository;
 import dev.example.aero.repository.FlightScheduleRepository;
 import dev.example.aero.repository.PassengerRepository;
 import dev.example.aero.repository.TicketRepository;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -36,14 +44,22 @@ public class FlightScheduleService {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "No Flight Inserted");
 
         for (String flightId : flightIDs) {
-            Flight flight = flightRepository.findById(flightId).orElse(null);
+            addOrUpdateFlightSchedule(flightDate, flightId);
+        }
+    }
 
-            if (flight != null) {
-                List<FlightSchedule> flightSchedules = flight.getSeatInfoList().stream()
-                        .map(seatInfo -> new FlightSchedule(flightDate, flightId, seatInfo.getSeatClassType(), seatInfo.getAvailableSeats(), seatInfo.getFare()))
-                        .collect(Collectors.toList());
-                flightScheduleRepository.saveAll(flightSchedules);
-            }
+    private void addOrUpdateFlightSchedule(LocalDate flightDate, String flightID) {
+        if (flightID == null || flightID.isEmpty())
+            return;
+
+        Flight flight = flightRepository.findById(flightID).orElse(null);
+
+        if (flight != null) {
+            List<FlightSchedule> flightSchedules = flight.getSeatInfoList().stream()
+                    .map(seatInfo -> new FlightSchedule(flightDate, flightID, seatInfo.getSeatClassType(), seatInfo.getAvailableSeats(), seatInfo.getFare()))
+                    .collect(Collectors.toList());
+            System.out.println("Saving schedules...");
+            flightScheduleRepository.saveAll(flightSchedules);
         }
     }
 
