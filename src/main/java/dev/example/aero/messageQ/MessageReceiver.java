d33cf780c6cc6accc176be0e7ee8b000944b8fad
SkAditAziz <skaditaziz@gmail.com>
2024-01-30 00:45:42 +0600

    Enabled messageQ to receive different type of messages

--- src/main/java/dev/example/aero/messageQ/MessageReceiver.java
+++ src/main/java/dev/example/aero/messageQ/MessageReceiver.java
@@ -1,9 +1,12 @@
 package dev.example.aero.messageQ;
 
+import dev.example.aero.model.Ticket;
 import dev.example.aero.model.TicketWrapper;
 import dev.example.aero.service.TicketService;
 import dev.example.aero.service.utilService.EmailService;
 import dev.example.aero.util.EmailBuilder;
+import jakarta.jms.Message;
+import jakarta.jms.ObjectMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
@@ -18,10 +21,30 @@ public class MessageReceiver {
     private TicketService ticketService;
 
     @JmsListener(destination = "messagequeue.q")
+    public void processMessage(Message message) {
+        try {
+            ObjectMessage objectMessage = (ObjectMessage) message;
+            Object object = objectMessage.getObject();
+            if (object instanceof TicketWrapper) {
+                saveTicketAndSendMail((TicketWrapper) object);
+            }
+            else if (object instanceof Ticket) {
+                sendCancelMail((Ticket) object);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public void saveTicketAndSendMail(TicketWrapper ticketWrapper) {
         String filePath = ticketService.saveTicketPdf(ticketWrapper.getTicket(), ticketWrapper.getPdfTicket());
 
         Map<String, String> emailDetails = EmailBuilder.getConfirmationEmailDetails(ticketWrapper.getTicket());
-        emailService.sendEmailWithAttachment(emailDetails.get("to"), emailDetails.get("sub"), emailDetails.get("body"), filePath);
+        emailService.sendEmail(emailDetails.get("to"), emailDetails.get("sub"), emailDetails.get("body"), filePath);
+    }
+
+    public void sendCancelMail(Ticket ticket) {
+        Map<String, String> emailDetails = EmailBuilder.getCancelEmailDetails(ticket);
+        emailService.sendEmail(emailDetails.get("to"), emailDetails.get("sub"), emailDetails.get("body"));
     }
 }
